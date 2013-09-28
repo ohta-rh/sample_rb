b02a4c9ff544df090d9333f52d6dd33cbab9965c
ohta <ohta@raw-hide.co.jp>
2013-09-28 14:03:34 +0900

    sunao

---
+++ sunao.rb
@@ -0,0 +1,14 @@
+class Sunao
+
+  def say( type_code )
+
+    case type_code
+    when "rei"
+      print "あなたが死んでもかわりがいるもの\n"
+    when "asuka"
+      print "あんたばかあ？\n"
+    when "gendou"
+      print "乗るならはやくしろ、でなければかえれ\n"
+    end
+  end
+end
